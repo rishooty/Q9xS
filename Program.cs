66a101496f2dbc71f440a4461889f8d4c7f11945
Nicholas Ricciuti <rishooty@gmail.com>
2019-09-06 20:50:00 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using DiscUtils;
+using DiscUtils.Iso9660;
+
+namespace Q9xS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("One of your arguments are missing");
+                Console.WriteLine("The correct syntax is: dotnet Q9xS updatePath isoPath bootImagePath");
+                Environment.Exit(0);
+            }
+
+            Program self = new Program();
+            bool extract = true;
+            string updatePath = args[0];
+            string isoPath = args[1];
+            string bootImage = args[2];
+            string extractedIsoPath = Path.GetFileNameWithoutExtension(isoPath);
+
+            if (Directory.Exists(extractedIsoPath))
+            {
+                Console.WriteLine(@"It looks like you've already extracted the iso. Would you like to re-extract? [y/any other key]");
+                string response = Console.ReadLine();
+
+                if (!string.Equals(response, "y", StringComparison.OrdinalIgnoreCase))
+                    extract = false;
+            }
+
+            if(extract)
+                self.ExtractISO(isoPath, extractedIsoPath);
+            self.Update9xDir(updatePath, extractedIsoPath);
+            self.CreateISO(extractedIsoPath, bootImage);
+        }
+
+        public void Update9xDir(string updatesDir, string extracted9xDir) {
+            string subDir = null;
+            if (Directory.Exists(extracted9xDir+@"\Win95"))
+                subDir = extracted9xDir + @"\Win95";
+            else if (Directory.Exists(extracted9xDir+@"\Win98"))
+                subDir = extracted9xDir + @"\Win98";
+            else if (Directory.Exists(extracted9xDir+@"\Win9x"))
+                subDir = extracted9xDir + @"\Win9x";
+            else
+            {
+                Console.WriteLine(extracted9xDir + " is not an extracted windows iso");
+                Environment.Exit(0);
+            }
+
+            foreach(string update in Directory.GetFiles(updatesDir))
+            {
+                string copyTo = subDir + @"\" + Path.GetFileName(update);
+                FileInfo updateInfo = new FileInfo(update);
+                FileInfo copyToInfo = new FileInfo(copyTo);
+                if (!File.Exists(copyTo) || updateInfo.LastWriteTime > copyToInfo.LastWriteTime)
+                {
+                    updateInfo.CopyTo(copyToInfo.FullName, true);
+                    Console.WriteLine(updateInfo.FullName + " was copied to " + copyToInfo.FullName);
+                }
+            }
+        }
+
+        public CDBuilder AddToIso(CDBuilder builder, string dirToIso)
+        {
+            foreach (string directory in Directory.GetDirectories(dirToIso))
+            {
+                AddToIso(builder, directory);
+            }
+            foreach (string file in Directory.GetFiles(dirToIso))
+            {
+                Stream file2add = File.Open(file, FileMode.Open);
+                string file2addPath = file.Substring(file.IndexOf(@"\", 1) + 1);
+                builder.AddFile(file2addPath, file2add);
+                Console.WriteLine(file2addPath + " was added to the iso.");
+            }
+            return builder;
+        }
+
+        public void CreateISO(string dirToIso, string bootImagePath)
+        {
+            CDBuilder builder = new CDBuilder();
+            builder.UseJoliet = true;
+            builder.VolumeIdentifier = "WIN_9X";
+
+            if (!File.Exists(bootImagePath))
+            {
+                Console.WriteLine("Boot image does not exist, exiting.");
+                Environment.Exit(0);
+            }
+
+            Stream boot = File.Open(bootImagePath, FileMode.Open);
+            builder.SetBootImage(boot, BootDeviceEmulation.Diskette1440KiB, 0);
+
+            builder = AddToIso(builder, dirToIso);
+
+            if (Directory.Exists(dirToIso + @"\[BOOT]"))
+                Directory.Delete(@"\[BOOT]", true);
+
+            builder.Build(Path.GetFileNameWithoutExtension(dirToIso)+".iso");
+            Console.WriteLine(Path.GetFileNameWithoutExtension(dirToIso+".iso succesfully updated."));
+        }
+
+        public void ExtractISO(string toExtract, string folderName)
+        {
+            CDReader Reader = new CDReader(File.Open(toExtract, FileMode.Open), true);
+            ExtractDirectory(Reader.Root, folderName + "\\", "");
+            Console.WriteLine(toExtract + " was succesfully extracted.");
+            Reader.Dispose();
+        }
+
+        public void ExtractDirectory(DiscDirectoryInfo Dinfo, string RootPath, string PathinISO)
+        {
+            if (!string.IsNullOrWhiteSpace(PathinISO))
+            {
+                PathinISO += "\\" + Dinfo.Name;
+            }
+            RootPath += "\\" + Dinfo.Name;
+            AppendDirectory(RootPath);
+            foreach (DiscDirectoryInfo dinfo in Dinfo.GetDirectories())
+            {
+                ExtractDirectory(dinfo, RootPath, PathinISO);
+            }
+            foreach (DiscFileInfo finfo in Dinfo.GetFiles())
+            {
+                using (Stream FileStr = finfo.OpenRead())
+                {
+                    using (FileStream Fs = File.Create(RootPath + "\\" + finfo.Name))
+                    {
+                        FileStr.CopyTo(Fs, 4 * 1024);
+                        Console.WriteLine(finfo.Name + " was extracted.");
+                    }
+                }
+            }
+        }
+
+        static void AppendDirectory(string path)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+            catch (DirectoryNotFoundException Ex)
+            {
+                AppendDirectory(Path.GetDirectoryName(path));
+            }
+            catch (PathTooLongException Ex)
+            {
+                AppendDirectory(Path.GetDirectoryName(path));
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,97 @@
+﻿# Quick 9x Slipstreamer
+
+A utility designed to ease the process of slipstreaming updates into a Windows 95/98/ME iso.
+
+## Why
+
+It's possible to slipstream some updates into Windows 9x series OSes so long as they match
+either files that already exist on the disc or within its .CAB files.
+
+However, I found the process of extracting the iso, dropping the files, and recompressing a bootable iso tedious.
+
+* It can take a bit to load all the files into your iso creating application, such as CDBurnerXP.	
+* Not all slipstreams are successful. There are many files that if modified will cause
+	  the OS installer to either simply not start or fail partway through. Given this fact,
+	  knowing which files you can and can't slipstream is a matter of trial and error.
+
+As for "Why Windows 9x?", yes there are people that still use these OSes. Namely, vintage gaming enthusiasts.
+
+## Doesn't this exist already?
+Yes, but it's out of date and won't run on modern OSes. I've tried to employ the similar method of directly
+slipstreaming into the .CAB files, but it didn't work out for a number of reasons. 
+	
+* The best method I found involved using external tools and wasn't exactly cross-platform. 
+* All of the available C# libraries couldn't extract split cabs individually.
+* There are some .CABs windows doesn't even like being rebuilt at all, even with no changes.
+* Driver related updates don't need to match existing files, and would be missed if I used the above method. MSBATCH.inf included.
+* Encouraging people to automatically update a large mass of files didn't seem like a good idea in general. Slipstreaming is 
+  already finicky as it is, so the user should know exactly which files they're updating and why.
+
+## Getting Started
+
+### Prerequisites
+
+#### User
+
+* Windows 7 or higher
+	* [.NET Core Runtimes](https://dotnet.microsoft.com/download/dotnet-core/2.1)
+* Linux with .NET Core 2.1 support. See [here](https://docs.microsoft.com/en-us/dotnet/core/linux-prerequisites?tabs=netcore2x) for details.
+
+#### Dev
+* [Visual Studio 2017 15.7 or higher](https://visualstudio.microsoft.com/vs/)
+* [.NET Core SDK](https://dotnet.microsoft.com/download/dotnet-core/2.1)
+
+### Setup
+Download the latest release [here](WIP), and extract it to the directory of your choice.
+
+## Setting up your updates
+
+Create a directory in which to extract all your updates. No subdirectories, zips, cabs, or even self-extracting
+exes should be in this folder. Use [7-zip](https://www.7-zip.org/) or a similar archive utility to extract them.
+
+Take Q216204.exe for example, an unofficial update which fixes detection of some Intel cpus.
+Dropping the file itself into this directory wouldn't work, but extracting its contents would.
+
+Keep and overwrite files in this folder as you see fit. You may want the latest versions,
+or you may want to keep a specific version of a given file. Just make sure that everything
+is a plain file or executable.
+
+## Running
+
+Windows:
+```
+dotnet Q9xS.dll updatesDirectory isoPath bootImagePath
+```
+
+Linux:
+```
+dotnet ./Q9xS.dll updatesDirectory isoPath bootImagePath
+```
+
+Example:
+```
+dotnet Q9xS.dll C:\9x\updates "C:\9x\Windows 98 Second Edition.iso" 
+```
+
+When it's finished, an updated iso will be in the same directory as the executable.
+Because slipstreaming is so finicky, I would try it out in a virtual machine
+before attempting it on the real thing. Chances are that if something goes wrong,
+you just need to delete whichever file the installer gives you an error message for.
+If not that, something related to it.
+
+Have fun customizing your 9x discs!
+
+## Built With
+
+* .NET Core 2.1
+* Visual Studio 16.2.3
+* [DiscUtils](https://github.com/DiscUtils/DiscUtils)
+* [Markdown by Hey-Red](https://github.com/hey-red/Markdown)
+
+## Author
+
+**Nicholas Ricciuti** - [rishooty](https://github.com/rishooty)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
